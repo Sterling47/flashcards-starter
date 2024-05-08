e391381240eed9dd7588f7bd745379ac606bc8b3
Adam Konber <konber3@gmail.com>
2024-05-07 21:07:06 -0600

    complete createDeck and countCard functions

---
+++ test/deck-test.js
@@ -0,0 +1,41 @@
+const chai = require('chai');
+const expect = chai.expect;
+const { createDeck, countCards } = require('../src/deck');
+const { createCard } = require('../src/card');
+
+describe('Deck', function() {
+    it('should return an empty array', function(){
+        const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+
+        const card2 = createCard(2, "What is a comma-separated list of related values?", ["array", "object", "function"], 'array')
+        const deck = createDeck()
+
+        expect(deck).to.deep.equal([])
+    })
+    
+    it('should be able to create a deck and add cards to the deck', function () {
+        const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+
+        const card2 = createCard(2, "What is a comma-separated list of related values?", ["array", "object", "function"], 'array')
+        const deck = createDeck(card, card2)
+
+        expect(deck.length).to.equal(2)
+        expect(deck).to.include(card)
+        expect(deck).to.include(card2)
+    })
+
+    it('should be able to count how many cards are in the deck', function(){
+        const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+
+        const card2 = createCard(2, "What is a comma-separated list of related values?", ["array", "object", "function"], 'array')
+        const deck = createDeck(card, card2)
+        const count = countCards(deck)
+
+        expect(count).to.deep.equal(2)
+    })
+    
+}) 
+
+
+
+
